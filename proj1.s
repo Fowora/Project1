207ce55cacb16ef9b15888c5353ddbf25f223552
Emmanuel Fowora <43800038+Fowora@users.noreply.github.com>
2019-10-31 03:09:20 -0400

    Mips change to loop method

--- proj1.s
+++ proj1.s
@@ -3,17 +3,9 @@
 string: .asciiz "Enter a string of 10 characters:"
 .text
 main:
-li $v0, 8
-la $a0, string
-syscall
-li $v0, 4
-syscall
-
-addi $t0,$t0, 0
-Counter: bne $t0, 10, Exit
-	la $a1,0($a0)
-
-	addi $a1,$a0,1
-	addi $t0,$t0,1
-	j Counter
+li $v0, 8 #used to take input from a user
+la $a0, string #used to save the string in the variable
+syscall #used to print the string
+loop: #Decided to use a loop instead of a counter 
+#using to test github
 Exit:
